d6b93fdf95afdc9ac81186d72d9f27661ce4ee36
nalhin <krol.wow@gmail.com>
2020-11-12 11:59:57 +0100

    feat: groovy setup

---
+++ backend/src/test/groovy/com.nalhin.financecalculator/ApplicationTests.groovy
@@ -0,0 +1,14 @@
+package com.nalhin.financecalculator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
