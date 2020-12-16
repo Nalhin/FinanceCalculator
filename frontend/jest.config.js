225dfd76d67008604010aa51768e40697e1cbe7a
Krzysztof Olipra <contact@krzysztofolipra.com>
2020-12-16 12:43:44 +0100

    ci: sonarcloud config.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   transform: {
     '^.+\\.[t|j]sx?$': 'babel-jest',
   },
+
+  reporters: ['default', 'jest-sonar'],
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
   setupFilesAfterEnv: ['./test/setup.ts'],
 };
